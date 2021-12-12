75c03db6a1e447540156c1db8e2683eaf918955b
Arai Shota <me@arsho.dev>
2021-12-12 16:11:22 +0900

    is:closed -> is:merged

--- lib/git/pr/release/cli.rb
+++ lib/git/pr/release/cli.rb
@@ -125,7 +125,7 @@ module Git
         def fetch_merged_pr_numbers_from_github
           git(:log, '--pretty=format:%H', "origin/#{production_branch}..origin/#{staging_branch}").map(&:chomp).map do |sha1|
             sleep 1
-            client.search_issues("repo:#{repository} is:pr is:closed #{sha1}")[:items].map(&:number)
+            client.search_issues("repo:#{repository} is:pr is:merged #{sha1}")[:items].map(&:number)
           end.flatten
         end
 
